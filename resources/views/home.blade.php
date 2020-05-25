a489b5712f135c9786d15673029ef04297e32995
sathapornma <sathaporn.ma@rmuti.ac.th>
2020-05-25 13:43:49 +0700

    priority preemptive

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -39,23 +39,7 @@
     </div>
     <hr>
     <h4>Gantt Chart</h4>
-    <div class="progress">
-        
-    </div>
-
-    <div class="row">
-        <div class="col-12">
-            <div class="gantt-chart table-responsive">
-                <table class="table table-striped table-dark text-center tchart">
-                    <tbody>
-                        <tr class="tgantt-chart"></tr>
-                        <tr class="tnumgantt-chart"></tr>
-                    </tbody>
-                </table>
-            </div>
-        </div>
-        
-    </div>
+    <div class="progress"></div>
     
     <b >CPU Time : <span id="cpuStartTime"> - </span></b><br>
     <b >CPU End Time : <span id="cpuEndTime"> - </span></b><br>
